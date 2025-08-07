3800680c595ae0763559bbf4d99bdc858b056dd9
Lexa2805 <165327278+Lexa2805@users.noreply.github.com>
2025-08-07 18:11:56 +0300

    ceva

---
+++ module1/examples1.py
@@ -0,0 +1,22 @@
+#create triangle
+print("    *    ")
+print("   * *   ")
+print(" *     * ")
+print("* * * * *")
+
+#create something
+print("---------")
+print("|       |")
+print("|       |")
+print("---------")
+
+print(type (10 // 3 ))
+
+print(True and False)
+print('a' and 'b')
+
+a = 6
+b = 3
+a /= 2 * b
+print(a)
+
---
+++ module1/functions.py
@@ -0,0 +1,16 @@
+print('Hello Python','New Modul',sep=' ',end='\n')
+
+#Data type string
+
+string='Hello Python'
+string1="Hello Python1"
+string2='''
+Hello Python2
+'''
+string3="""Hello Python3"""
+string4="Hello Python4"
+string5=r"Hello Python5"
+string6="\nHello Python6\n"
+print(string6)
+string7=f"Hello Python7"
+print(string7)
